344f834a1c1bb870fbee8e884228cb981fa11e2d
unknown <nschimka@yahoo.com>
2025-04-27 21:51:36 -0500

    one up

--- 573/src/visualization/get_models.sh
+++ 573/src/visualization/get_models.sh
@@ -9,4 +9,4 @@ pip install AlignScore/.
 python -m spacy download en_core_web_sm
 wget https://huggingface.co/yzha/AlignScore/resolve/main/AlignScore-base.ckpt -P ./AlignScore
 
-cd ../../../
+cd ../../../../
